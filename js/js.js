70841f045b9a0e880b5afb9a1c1b39d68f7380c4
Anastasiia-derymarko <lolololo2263@gmail.com>
2018-06-09 17:10:44 +0300

    new

---
+++ js/js.js
@@ -0,0 +1,50 @@
+
+//  массив доствки 
+var delivery = [
+		
+		{
+			"name": "Платформа метро",
+			"hint-block":"При доставці замовлення кур’єром до станції метро (платформа) – безкошовно."
+			"images" :
+		}
+
+]
+var output = document.getElementsByTagName('output')[0];
+
+var out = '';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('circle').onmouseover = function(){mouseOver()};
+document.getElementById('circle').onmouseout = function(){mouseOut()};
+
+function mouseOver(){
+	document.getElementById('hint-block').style.display="block";
+
+}	
+
+function mouseOut(){
+	document.getElementById('hint-block').style.display='none'; 
+
+}	
